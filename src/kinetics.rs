e8cb20f4e0b69e0e38ef22320bba139b0374d22f
Rayhan Alghiffari Azizi <rayhan.azizi9@gmail.com>
2023-08-06 17:31:31 +0700

    add constructor

--- src/kinetics.rs
+++ src/kinetics.rs
@@ -57,8 +57,8 @@ impl ops::Mul<ReactorPWRData> for f64 {
 #[wasm_bindgen]
 pub struct ReactorPWR {
     data: ReactorPWRData,
-    pub time: f64,
-    pub dt: f64,
+    time: f64,
+    dt: f64,
     // neutronic
     beta: [f64; 6],
     Beta : f64,
@@ -74,9 +74,47 @@ pub struct ReactorPWR {
 
 #[wasm_bindgen]
 impl ReactorPWR {
+    // initialize
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        // for now, hardcode the value
+        let beta = [0.000215, 0.001424, 0.001274, 0.002568, 0.000748, 0.000273];
+        let lambda = [0.0124, 0.0305, 0.111, 0.301, 1.14, 3.01];
+        let time = 0.0;
+        let dt   = 1e-3;
+        let Lambda = lambda.iter().sum();
+        let Beta = beta.iter().sum();
+        let excess_reactivity = 0.0;
+        let external_reactivity = 0.0;
+        let alpha_fuel = 0.0;
+        let alpha_coolant = 0.0;
+        let data = ReactorPWRData {
+            neutron: 1e8,
+            precursors: [0.0; 6],
+            reactivity: 0.0,
+            temp_fuel: 0.0,
+            temp_coolant: 0.0,
+        };
+        ReactorPWR {
+            data, time, dt, beta, Beta, lambda, Lambda,
+            excess_reactivity, external_reactivity, alpha_fuel, alpha_coolant,
+        }
+    }
+
+    pub fn get_datas(&self) -> Vec<f64> {
+        let mut datas = Vec::new();
+        datas.push(self.data.neutron);
+        datas.extend_from_slice(&self.data.precursors);
+        datas.push(self.data.reactivity);
+        datas.push(self.data.temp_fuel);
+        datas.push(self.data.temp_coolant);
+        return datas;
+    }
+
     // calculate the differential equations (d/dt data)
-    pub fn calc_diff(&self, d : ReactorPWRData) -> ReactorPWRData{
+    fn calc_diff(&self, d : ReactorPWRData) -> ReactorPWRData{
         // differential equations
+        // TODO : termperature
         let reactivity = self.excess_reactivity + self.external_reactivity +
             self.alpha_fuel * d.temp_fuel + self.alpha_coolant * d.temp_coolant;
         let sum_lambdaC : f64 = self.lambda.iter().zip(d.precursors.iter()).map(|(x,y)| x*y).sum();
@@ -94,10 +132,17 @@ impl ReactorPWR {
         };
     }
 
-    pub fn calc_step_euler(&self, d : ReactorPWRData, dt : f64) -> ReactorPWRData {
+    fn calc_step_euler(&self, d : ReactorPWRData, dt : f64) -> ReactorPWRData {
         // solve using euler's method
         let ddata = self.calc_diff(d);
         return d + ddata * dt;
+    }
+
+    pub fn step_euler(&mut self) {
+        // calculate the next step
+        let next_data = self.calc_step_euler(self.data, self.dt);
+        self.data = next_data;
+        self.time += self.dt;
     }
 }
 
